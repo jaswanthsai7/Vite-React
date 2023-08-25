fcf650cba6833000b313c985987f1b50ad038dd3
Jaswanth Sai <jasvanthsai7@gmail.com>
2023-08-25 13:19:02 +0530

    formatted the code

--- vite-react-poster/src/components/Post.jsx
+++ vite-react-poster/src/components/Post.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import classes from "./Post.module.css";
 const names = ["Tony", "Strange"];
 
-export default function Post({author,body,deletePost,id}) {
+export default function Post({ author, body, deletePost, id }) {
   const chooseName = Math.random() > 0.5 ? names[0] : names[1];
- const deleteCurrPost=()=>{
-  deletePost(id);
- }
-//  console.log(...props.post)
+  const deleteCurrPost = () => {
+    deletePost(id);
+  };
+  //  console.log(...props.post)
   return (
     <li className={classes.post}>
       <p className={classes.author}>{author}</p>
       <p className={classes.text}>{body}</p>
-      <br/>
-      <button className={classes.button} onClick={deleteCurrPost}>Delete</button>
+      <br />
+      <button className={classes.button} onClick={deleteCurrPost}>
+        Delete
+      </button>
     </li>
   );
 }
