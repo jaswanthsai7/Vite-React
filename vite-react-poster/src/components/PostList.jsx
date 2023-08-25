d3f2cf73db5b207869657c47c22c8f4f5da7a66b
Jaswanth Sai <jasvanthsai7@gmail.com>
2023-08-25 11:54:20 +0530

    connected it to firebase realtime database

--- vite-react-poster/src/components/PostList.jsx
+++ vite-react-poster/src/components/PostList.jsx
@@ -1,15 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
+import { useCallback } from "react";
 
 export default function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
-  function addPostHandler(postData) {
-    setPosts((prevData) => [postData, ...prevData]);
-  }
+  const addPostHandler = async (postData) => {
+    await fetch(
+      "https://vite-react-poster-default-rtdb.firebaseio.com/posts.json",
+      {
+        method: "POST",
+        body: JSON.stringify(postData),
+        headers: {
+          "Conntent-Type": "application/json",
+        },
+      }
+    ).catch((error) => {
+      console.error(error);
+    });
+    loadData();
+    // setPosts((prevData) => [postData, ...prevData]);
+  };
+
+  const loadData = useCallback(() => {
+    fetch("https://vite-react-poster-default-rtdb.firebaseio.com/posts.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const loaded = [];
+        for (const key in data) {
+          loaded.push({
+            id: key,
+            body: data[key].body,
+            author: data[key].author,
+          });
+        }
+        setPosts(loaded);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    loadData();
+    console.log(1);
+  }, [loadData]);
 
   let modalContent;
   if (isPosting) {
